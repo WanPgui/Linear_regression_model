67dbb032e3825aca41b0a57b39713c2b762c78ce
Pwangui <127877940+WanPgui@users.noreply.github.com>
2024-11-24 00:48:50 +0300

    Update api_service.dart

--- summative/FlutterAppHealthPredictionRisk/cvd_prediction_app/lib/api_service.dart
+++ summative/FlutterAppHealthPredictionRisk/cvd_prediction_app/lib/api_service.dart
@@ -4,8 +4,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class MyApiService {
-  final String apiUrl =
-      'https://4249-102-0-13-76.ngrok-free.app/predict'; // Replace with your actual API URL
+  final String apiUrl = 'https://78a5-102-0-13-76.ngrok-free.app/predict';
 
   Future<String> predictHealthRisk(Map<String, dynamic> data) async {
     final response = await http.post(
